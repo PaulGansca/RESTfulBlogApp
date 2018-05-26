3e912fd836b1795ed8d0d4ff64438fdb24918015
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-26 20:16:48 +0000

    Added EDIT and UPDATE routes, using _method ovveride on the html Form

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
 
 //App config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs")
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //Mongoose schema config:title, image, body, date created
 var blogSchema = new mongoose.Schema ({
@@ -50,6 +52,39 @@ app.post("/blogs", function(req, res) {
            res.redirect("/blogs")
        }
    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
