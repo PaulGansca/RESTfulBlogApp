1292f73c8577e7c5c2592bd4b5678b93bed2f2b2
Paul Gansca <gansca.paul@gmail.com>
2019-02-20 09:51:41 +0000

    Connected server to DB, save posts from compose route in db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+var mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,14 +19,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
+});
+
+const postsSchema = {
+  title: String,
+  body: String
+}
+
+const Post = mongoose.model("Post", postsSchema);
 
 //ROUTES
 
 app.get("/", (req, res) => {
-  res.render('home.ejs', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  //get all posts to render
+  Post.find((err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home.ejs', {
+        homeStartingContent: homeStartingContent,
+        posts: results
+      });
+    }
   });
 });
 
@@ -46,11 +66,12 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  let post = {
+  let post = new Post({
     title: req.body.postTitle,
     body: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save();
+
   res.redirect("/");
 });
 
